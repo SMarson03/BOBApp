f886704be7aa5dcd6033f1cab3e667cc4cce7392
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2026-07-19 19:04:02 -0400

    update dash unddoo

--- src/components/admin/DashboardStats.jsx
+++ src/components/admin/DashboardStats.jsx
@@ -3,56 +3,42 @@ function DashboardStats({ places, categories, locations }) {
     <div className="grid grid-cols-4 gap-4 mb-6">
 
       <div className="border-2 border-black bg-yellow-100 p-4">
-        <p className="text-sm">Businesses</p>
+        <p className="text-sm">
+          Businesses
+        </p>
+
         <h2 className="text-3xl font-bold">
           {places.length}
         </h2>
       </div>
 
+
       <div className="border-2 border-black bg-yellow-100 p-4">
-        <p className="text-sm">Categories</p>
+        <p className="text-sm">
+          Categories
+        </p>
 
-        {categories.map(category => (
-          <div
-            key={category}
-            className="flex justify-between text-sm mt-1"
-          >
-            <span>{category.replaceAll("_", " ")}</span>
-
-            <span>
-              {
-                places.filter(
-                  place => place.category === category
-                ).length
-              }
-            </span>
-          </div>
-        ))}
+        <h2 className="text-3xl font-bold">
+          {categories.length}
+        </h2>
       </div>
 
+
       <div className="border-2 border-black bg-yellow-100 p-4">
-        <p className="text-sm">Boroughs</p>
+        <p className="text-sm">
+          Boroughs
+        </p>
 
-        {locations.map(location => (
-          <div
-            key={location}
-            className="flex justify-between text-sm mt-1"
-          >
-            <span>{location}</span>
-
-            <span>
-              {
-                places.filter(
-                  place => place.location === location
-                ).length
-              }
-            </span>
-          </div>
-        ))}
+        <h2 className="text-3xl font-bold">
+          {locations.length}
+        </h2>
       </div>
 
+
       <div className="border-2 border-black bg-yellow-100 p-4">
-        <p className="text-sm">Today</p>
+        <p className="text-sm">
+          Today
+        </p>
 
         <h2 className="text-xl font-bold">
           {new Date().toLocaleDateString()}
